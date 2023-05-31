a8c011f378d44c436427013d2ab8b3fee3814256
ahamedsuhails <suhias020official@gmail.com>
2023-05-31 09:12:05 +0530

    adding middelwares

--- app.js
+++ app.js
@@ -3,14 +3,18 @@ const PORT = 3000;
 const taskRouter = require("./routes/taskRoutes");
 const connectDB = require("./db/dbConnect");
 require("dotenv").config();
+const notFound = require("./middleware/not-found");
 
 const app = express();
 
+//middleware to serve static files
 app.use(express.static("./public"));
 //parsing middleware
 app.use(express.json());
 //routes
 app.use("/api/v1/tasks", taskRouter);
+
+app.use(notFound);
 
 const start = async () => {
   try {
